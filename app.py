e5b0419a77945daa06197df7ba1ec29b2ceddce2
a385868-ctrl <a385868@uach.mx>
2025-10-29 08:34:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ if 'count' not in st.session_state:
   st.session_state['count'] = 0
   
 if 'name' not in st.session_state:
-  st.session_state['name'] = 'nombre'
-
+  st.session_state['name'] = Nombre
 if st.button("Click me"):
   st.session_state['count'] += 1
 
